4bd01e54abdeeaf09b2764bc79b160e1ae89b042
Bianca <bianbdecima@gmail.com>
2024-03-11 21:48:33 -0300

    annotations correction

--- final_project/database/SqlHandler.cs
+++ final_project/database/SqlHandler.cs
@@ -11,7 +11,5 @@ namespace final_project
     {
         public static string ConnectionString { get; set; }
 
-        //Data Source=DESKTOP-7LES993;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;
-
     }
 }
